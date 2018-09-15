c1b4df096e4042ba9ca11c4aa06ed74a555a0b6b
tyln <taylan.sbrcn@gmail.com>
2018-09-15 15:33:39 +0300

    provider check added

--- quickbadger/src/main/java/com/raqun/quickbadger/ext/ContextExt.kt
+++ quickbadger/src/main/java/com/raqun/quickbadger/ext/ContextExt.kt
@@ -4,3 +4,5 @@ import android.content.Context
 import android.content.Intent
 
 fun Context.canResolveBroadcast(intent: Intent) = packageManager.queryBroadcastReceivers(intent, 0).isNotEmpty()
+
+fun Context.providerExists(providerName:String) = packageManager.resolveContentProvider(providerName,0) != null
