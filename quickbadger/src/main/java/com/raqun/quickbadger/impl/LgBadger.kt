c572cebf45860b2b4f97fbafaf1c1f164bced4f2
tyln <taylan.sbrcn@gmail.com>
2018-09-15 15:36:15 +0300

    badger impl classes added

---
+++ quickbadger/src/main/java/com/raqun/quickbadger/impl/LgBadger.kt
@@ -0,0 +1,24 @@
+package com.raqun.quickbadger.impl
+
+import android.content.ComponentName
+import android.content.Context
+import com.raqun.easybadger.Badger
+
+class LgBadger : Badger {
+    override fun showBadge(context: Context, count: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun dismissBadge(context: Context) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getSupportedLaunchers(): List<String> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setComponentName(componentName: ComponentName) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
