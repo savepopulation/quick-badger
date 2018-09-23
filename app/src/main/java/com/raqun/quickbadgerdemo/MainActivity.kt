2df661d94ffd974900340219c21adaed326aa243
tyln <taylan.sbrcn@gmail.com>
2018-09-23 11:17:13 +0300

    sample added

--- app/src/main/java/com/raqun/quickbadgerdemo/MainActivity.kt
+++ app/src/main/java/com/raqun/quickbadgerdemo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.raqun.quickbadgerdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.raqun.quickbadger.QuickBadger
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Quick Badger Usage
+         */
+
+        QuickBadger.provideBadger(this)?.showBadge(this, 10)
     }
 }
